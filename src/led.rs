38a1efd7978238491d4917a551c88faa91de8fb4
codyn95 <cody.ngo95@yahoo.com>
2024-02-06 00:17:28 -0700

    console output for led

--- src/led.rs
+++ src/led.rs
@@ -1,7 +1,17 @@
 // use rppal;
-pub fn init_LED() {
+use console::style;
+pub fn init_led() {
     // Turn on the led
-    println!("LED on RED");
+    println!("{}", style("LED is RED").red());
 }
 
+pub fn led_adjust(r_value: u32, g_value: u32, b_value: u32) {
+    // Adjust pwm according to value
+    let r_hex = format!("{r_value:#04X}");
+    let g_hex = format!("{g_value:#04X}");
+    let b_hex = format!("{b_value:#04X}");
+    println!("Color being adjusted to: {} {} {}", style(r_hex).red(), style(g_hex).green(), style(b_hex).blue());
+
+    // TODO Output to pins to adjust LED color
+}
 
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,36 @@ mod led;
 
 // use rppal;
 use std::{env, time, thread};
-
+use text_io::read;
 fn main() {
     let mut start = false;
     println!("{}", env::consts::OS);
 
-    led::init_LED();
+    led::init_led();
+
     // Run loop check for button press to start
     while !start {
         println!("Waiting for start press");
         thread::sleep(time::Duration::from_millis(250));
+        // TODO Check for button press and set start to true
+        let input: String = read!("{}\n");
+        let trimmed_input = input.trim();
+        if trimmed_input == "start" {
+            start = true;
+        }
+        else {
+            println!("We got this {input}");
+        }
     }
+
+    // Button pressed start 1 hour timer
+    println!("Timer has started...");
+    // Start LED at blue
+    led::led_adjust(0, 0, 255);
+
+    // TODO LED start turning to red every hour
+    
+    // One hour limit reached
+    start = false;
+
 }
