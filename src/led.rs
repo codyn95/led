c4cb5c75387d94e6c45409bce43f00617e2d899f
codyn95 <cody.ngo95@yahoo.com>
2024-02-06 18:09:50 -0700

    color adjust on timer

--- src/led.rs
+++ src/led.rs
@@ -2,10 +2,18 @@
 use console::style;
 pub fn init_led() {
     // Turn on the led
-    println!("{}", style("LED is RED").red());
+    println!("{}", style("LED is Blue").blue());
 }
 
-pub fn led_adjust(r_value: u32, g_value: u32, b_value: u32) {
+pub fn led_adjust(mut r_value: u32, mut g_value: u32, mut b_value: u32) {
+    // Cap min/max value
+    if r_value > 255 {r_value = 255};
+    if g_value > 255 {g_value = 255};
+    if b_value > 255 {b_value = 255};
+    if r_value < 0 {r_value = 0};
+    if g_value < 0 {g_value = 0};
+    if b_value < 0 {b_value = 0};
+    
     // Adjust pwm according to value
     let r_hex = format!("{r_value:#04X}");
     let g_hex = format!("{g_value:#04X}");
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod led;
 
 // use rppal;
-use std::{env, time, thread, process};
+use std::{env, time::Duration, thread, process};
 use text_io::read;
+
+use crate::led::led_adjust;
 fn main() {
     let mut start = false;
+    let mut counter = 0;
     println!("{}", env::consts::OS);
 
     led::init_led();
@@ -12,13 +15,32 @@ fn main() {
     // Run loop check for button press to start
     loop {
         wait_loop();
-
+        counter = 0;
+        
         // Button pressed start 1 hour timer
         println!("Timer has started...");
-        // Start LED at blue
-        led::led_adjust(0, 0, 255);
+        
+        // Start LED at Green
+        led::led_adjust(0, 255, 0);
 
-        // TODO LED start turning to red every hour
+        // LED start turning to red every hour
+        let mut r_value = 0.0;
+        let mut g_value = 255.0;
+        let max_time = 3600;
+        while counter < max_time {
+            // At halfway time, LED is fully yellow
+            // At one hour, LED is fully red
+            if counter > max_time/2 {
+                g_value -= 255.0/max_time as f32;
+            }
+            
+            r_value += 255.0/(max_time as f32/2.0);
+            println!("{}", r_value.to_string());
+            led_adjust(r_value as u32, g_value as u32, 0);
+
+            thread::sleep(Duration::from_secs(1));
+            counter += 1;
+        }
 
         // One hour limit reached
         led::led_adjust(255, 0, 0)
@@ -28,7 +50,7 @@ fn main() {
 
 fn wait_loop(){
     println!("Waiting for start press");
-    thread::sleep(time::Duration::from_millis(250));
+    thread::sleep(Duration::from_millis(250));
     // TODO Check for button press
     loop {
         let input: String = read!("{}\n");
